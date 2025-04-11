0df8e098cf6f86f1b4d96a7bcb5325f687d8b6da
ProstoMakcc <antimaks1974@gmail.com>
2025-04-11 15:33:59 +0200

    Changed settings to deploy web app

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kiz_7%(q(ji)h-obp*aoqfzw#n5r+*bm6mf@!pjk^3z$7%0s5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
